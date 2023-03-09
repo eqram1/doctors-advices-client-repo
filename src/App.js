7112ea2ec1c978bc054a087b3e30718f98f272a4
eqram1 <abmeqramhossain@gmail.com>
2023-03-09 15:49:00 +0600

    updated button name with color

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div>
-      <button className="btn btn-outline">Button</button>
-      <button className="btn btn-outline btn-primary">Button</button>
-      <button className="btn btn-outline btn-secondary">Button</button>
-      <button className="btn btn-outline btn-accent">Button</button>
+      <button className="btn btn-outline btn-accent">Eqram</button>
+      <button className="btn btn-outline btn-secondary">Rina</button>
+      <button className="btn btn-outline btn-primary">Sakib</button>
+      <button className="btn btn-outline">Jawad</button>
     </div>
   );
 }
