dc1cb12b3e972ffe26628029cb0d4099a35409cb
eqram1 <abmeqramhossain@gmail.com>
2023-03-17 21:41:55 +0600

    added infocard with dynamic css class

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import InfoCards from '../InfoCards/InfoCards';
 
 
 
@@ -10,6 +11,7 @@ const Home = () => {
     return (
         <div className='mx-5'>
             <Banner></Banner>
+            <InfoCards></InfoCards>
         </div>
     );
 };
