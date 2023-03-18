1da8c5a6d4e9f02197fd2936ab5ef6b3347c4f06
eqram1 <abmeqramhossain@gmail.com>
2023-03-18 08:34:03 +0600

    added testimonials with avatarar and fake data

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 import InfoCards from '../InfoCards/InfoCards';
 import MakeAppointment from '../MakeAppointment/MakeAppointment';
 import Services from '../Services/Services';
+import Testimonial from '../Testimonial/Testimonial';
 
 
 
@@ -16,6 +17,7 @@ const Home = () => {
             <InfoCards></InfoCards>
             <Services></Services>
             <MakeAppointment></MakeAppointment>
+            <Testimonial></Testimonial>
         </div>
     );
 };
