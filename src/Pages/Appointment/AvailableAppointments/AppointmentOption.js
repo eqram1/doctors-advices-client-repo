57a3f2eb0fd566031ec6f16c4c041d43748fb587
eqram1 <abmeqramhossain@gmail.com>
2023-03-20 00:53:04 +0600

    added modal data with state inieractions

--- src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
+++ src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -10,6 +10,7 @@ const AppointmentOption = ({ appointmentOption, setTreatment }) => {
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'}</p>
                 <div className="card-actions justify-center">
                     <label
+                        disabled={slots.length === 0}
                         htmlFor="booking-modal" className="btn btn-primary text-white"
                         onClick={() => setTreatment(appointmentOption)}
                     >Book Appointment</label>
--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -2,8 +2,21 @@ import { format } from 'date-fns';
 import React from 'react';
 
 const BookingModal = ({ treatment, selectedDate }) => {
-    const { name, slots } = treatment;//treatment is appointment options,just different names.
+    //treatment is just another name of appointment options with (name,slots &-id)
+    const { name, slots } = treatment;
     const date = format(selectedDate, 'PP');
+
+    const handleBooking = event => {
+        event.preventDefault();
+        const form = event.target;
+        const slot = form.slot.value;
+        const name = form.name.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+
+        console.log(date, slot, name, email, phone);
+
+    }
     return (
         <>
             <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -11,17 +24,17 @@ const BookingModal = ({ treatment, selectedDate }) => {
                 <div className="modal-box relative">
                     <label htmlFor="booking-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 className="text-lg font-bold">{name}</h3>
-                    <form className='grid grid-cols-1 gap-3 mt-12'>
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-12'>
                         <input type="text" disabled value={date} className="input w-full input-bordered" />
-                        <select className="select select-bordered w-full">
+                        <select name="slot" className="select select-bordered w-full">
 
                             {
                                 slots.map(slot => <option value={slot}>{slot}</option>)
                             }
                         </select>
-                        <input type="text" placeholder="Type here" className="input w-full input-bordered" />
-                        <input type="text" placeholder="Type here" className="input w-full input-bordered" />
-                        <input type="text" placeholder="Type here" className="input w-full input-bordered" />
+                        <input name="name" type="text" placeholder="Your Name" className="input w-full input-bordered" />
+                        <input name="email" type="email" placeholder="Email Address" className="input w-full input-bordered" />
+                        <input name="phone" type="text" placeholder="Phone Number" className="input w-full input-bordered" />
                         <br />
                         <input className='btn btn-secondary w-full' type="submit" value="Submit" />
                     </form>
