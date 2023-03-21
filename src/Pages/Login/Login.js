b2a0b21b6a960dc66c5596354c3cd1d2f55d6559
eqram1 <abmeqramhossain@gmail.com>
2023-03-21 08:12:14 +0600

    updated error handling in login pages

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
@@ -6,6 +6,9 @@ const Login = () => {
     const { register, formState: { errors }, handleSubmit } = useForm()
     const handleLogin = data => {
         console.log(data);
+
+
+
     }
     return (
         <div className='h-[800px] flex justify-center items-center'>
@@ -16,19 +19,21 @@ const Login = () => {
                         <label className="label"><span className="label-text">Email</span></label>
                         <input type="text"
                             {...register("email", {
-                                required: "Email address is required"
+                                required: "Email Address is required"
                             })}
                             className="input input-bordered w-full max-w-xs" />
-                        {errors.email && <p role="alert">{errors.email?.message}</p>}
+                        {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Password</span></label>
                         <input type="password"
                             {...register("password", {
-                                required: true
+                                required: "Password is required",
+                                minLength: { value: 6, message: "Password must be 6 character or longer" }
                             })}
                             className="input input-bordered w-full max-w-xs" />
                         <label className="label"><span className="label-text">Forget Password?</span></label>
+                        {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
                     </div>
                     <input className='btn btn-primary w-full' value="Login" type="submit" />
                 </form>
