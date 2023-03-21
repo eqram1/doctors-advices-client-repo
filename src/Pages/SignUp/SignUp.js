4df480e8a544686198c945cea20d82d5d6bdac4a
eqram1 <abmeqramhossain@gmail.com>
2023-03-21 17:48:50 +0600

    added react hook form to create sign up form

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,24 +1,41 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const handleSignUp = (data) => {
+        console.log(data);
+        console.log(errors);
+    }
+
     return (
         <div className='h-[800px] flex justify-center items-center'>
             <div className='w-96 p-7'>
                 <h2 className='text-xl text-center'>Sign Up</h2>
-                <form>
+                <form onSubmit={handleSubmit(handleSignUp)}>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Name</span></label>
-                        <input type="text" className="input input-bordered w-full max-w-xs" />
+                        <input type="text" {...register("name", {
+                            required: "name is required"
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Email</span></label>
-                        <input type="text" className="input input-bordered w-full max-w-xs" />
+                        <input type="email" {...register("email", {
+                            required: true
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Password</span></label>
-                        <input type="password" className="input input-bordered w-full max-w-xs" />
-                        <label className="label"><span className="label-text">Forget Password?</span></label>
+                        <input type="password" {...register("password", {
+                            required: "password is required",
+                            minLength: { value: 6, message: "password must be 6 characters long" }
+
+                        })} className="input input-bordered w-full max-w-xs" />
+                        {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
                     </div>
                     <input className='btn btn-primary w-full' value="Sign Up" type="submit" />
                 </form>
