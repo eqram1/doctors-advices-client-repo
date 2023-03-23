1fd710839e6c1f9ce125613f93440445009b6df6
eqram1 <abmeqramhossain@gmail.com>
2023-03-23 19:26:30 +0600

    added private route and redirect user

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -5,6 +5,8 @@ import Dashboard from "../../Pages/Dashboard/Dashboard/Dashboard";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import SignUp from "../../Pages/SignUp/SignUp";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+
 
 const router = createBrowserRouter([
     {
@@ -32,7 +34,7 @@ const router = createBrowserRouter([
     },
     {
         path: '/dashboard',
-        element: <Dashboard></Dashboard>
+        element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>
     }
 ])
 export default router;
