9dc398dcc6529d0e18dc9bad86f8cdf790a6339f
eqram1 <abmeqramhossain@gmail.com>
2023-03-24 00:50:59 +0600

    updated private route and redirect user to the page they wanted to go

--- src/Contexts/AuthProvider.js
+++ src/Contexts/AuthProvider.js
@@ -7,16 +7,20 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
     const signIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
     const updateUser = (userInfo) => {
         return updateProfile(user, userInfo);
     }
     const logOut = () => {
+        setLoading(true);
         return signOut(auth);
     }
 
@@ -24,6 +28,7 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             console.log('user observing');
             setUser(currentUser);
+            setLoading(false);
         });
         return () => unsubscribe();
     }, [])
@@ -32,7 +37,8 @@ const AuthProvider = ({ children }) => {
         signIn,
         updateUser,
         logOut,
-        user
+        user,
+        loading
 
     }
     return (
