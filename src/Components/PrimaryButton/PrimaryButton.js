eb37f5d787ad9e14f3391654c4b51e2ac17b476d
Your Name <you@example.com>
2023-07-24 08:40:33 +0600

    updated some close file components

--- src/Components/PrimaryButton/PrimaryButton.js
+++ src/Components/PrimaryButton/PrimaryButton.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const PrimaryButton = ({ children }) => {
     return (
-        <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">{children}</button>
+        <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary text-white">
+            {children}</button>
     );
 };
 
